fbd4d6af5540390af9228a7536062e79457f58d6
OmkiranSG <omkiransg@gmail.com>
2020-04-20 16:24:00 +0530

    Squashed some more bugs

--- Flipkart_Tracker.py
+++ Flipkart_Tracker.py
@@ -21,11 +21,8 @@ iterate_rate = 0
 
 # https://www.flipkart.com/samsung-galaxy-s10-prism-white-128-gb/p/itmfdyp64j3hsfzy
 # https://www.flipkart.com/mi-led-smart-tv-4x-108-cm-43/p/itmab87244d2fead
-# https://www.flipkart.com/nikon-d5600-dslr-camera-body-single-lens-af-p-dx-nikkor-18-55-mm-f-3-5-5-6g-vr-16-gb-sd-card/p/itmezvbdzefvjpfn
 # https://www.flipkart.com/jbl-t450bt-extra-bass-bluetooth-headset-mic/p/itmf3vhgqqtat2eh
 # https://www.flipkart.com/redmi-note-7-pro-space-black-64-gb/p/itmfegkx2gufuzhp
-# https://www.flipkart.com/acer-predator-helios-300-core-i5-8th-gen-8-gb-1-tb-hdd-128-gb-ssd-windows-10-home-4-graphics-ph315-51-ph315-51-51v7-ph315-51-55xx-gaming-laptop/p/itm913399b8faf3d
-
 
 def popup():
 	print('Executing popup function')
@@ -33,7 +30,7 @@ def popup():
 	link = UIlink.get()
 	if 'flipkart' not in link.lower():
 		print('## Not Flipkart link ##')
-		tkinter.messagebox.showinfo('Invalid Link','Please Enter a Valid Link')
+		tkinter.messagebox.showwarning('Invalid Link','Please Enter a Valid Link')
 	else:
 		try:
 			print('Getting link data')
@@ -76,16 +73,15 @@ def popup():
 			print('Comp Price:' + str(comp_price))
 			global iterate_rate
 			iterate_rate = IntVar()
-			iterate_rate.set(1)		#Default set to 5 per day
+			iterate_rate.set(1)		#Default:1.Set to 5 per day 2.Demo 3 per minute
 			opt1 = ttk.Radiobutton(frame6, text='Iterate 5/day',command=radio, variable=iterate_rate, value=1)
 			opt1.pack(side=TOP, padx=20, pady=10)
 			opt2 = ttk.Radiobutton(frame6, text='Iterate 3/minute(Demo)',command=radio, variable=iterate_rate, value=2)
 			opt2.pack(side=BOTTOM, padx=20, pady=10)
 		except:
 			print('Link parsing Error')
-			tkinter.messagebox.showinfo('Invalid Link','Please Enter a Valid Link')
+			tkinter.messagebox.showerror('Invalid Link','Please Enter a Valid Link')
 		  
-
 	
 def price_compare():
 	global UIProd_link
@@ -93,11 +89,11 @@ def price_compare():
 	str_price = UIProd_link.get()
 	print('User Price:' + str_price)
 	global user_price
-	user_price = float(str_price)
+	user_price = float(str_price.replace(',', ''))
 	print('Comparing User Price')
 	if(comp_price >= user_price):
-		print(str(comp_price) + ' >= ' + str(user_price))
-		tkinter.messagebox.showinfo('Sh!t',f'You will be Notified when the price drops below ₹{str(user_price)}')
+		print(f'{comp_price} >= {user_price}')
+		tkinter.messagebox.showinfo('Sh!t',f'You will be Notified when the price drops below ₹{str_price}')
 	else:
 		print(f'Calling mail function: {str(comp_price)} < {str(user_price)}')
 		mail()
@@ -109,35 +105,35 @@ def mail():
 	mail_ID = UImail_Id.get()
 	print('Getting mailID')
 	print(mail_ID)
-	server =smtplib.SMTP('smtp.gmail.com',587)
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	server.login('#email','#password')     #Enter Service Mail_Id and Password
-	print('Cooking Subject and Message')
-	subject ='Flipkart Price Tracker:Price Drop Alert'
-	body =(f'The Price of the product has dropped to INR - {derkprice[1:]}.You can buy it at:\n {link}')
-	msg =f"Subject:{subject}\n\n{body}"
-	print('Sending Email')
-	server.sendmail('#email', mail_ID ,msg)        #Enter Mail_Id again
-	tkinter.messagebox.showinfo('Congrats',"The Email has been sent!")
-	print("Email has been sent")
-	server.quit()
+	if '@' not in mail_ID:
+		print('Invalid Email_ID')
+		tkinter.messagebox.showerror('Invalid Mail_Id','Please Enter a valid Email_ID')
+	else:
+		server = smtplib.SMTP('smtp.gmail.com',587)
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login('#email','#password')     #Enter Service Mail_Id and Password
+		print('Cooking Subject and Message')
+		subject ='Flipkart Price Tracker:Price Drop Alert'
+		body =(f'The Price of the product has dropped to INR - {derkprice[1:]}.You can buy it at:\n {link}')
+		msg =f"Subject:{subject}\n\n{body}"
+		print('Sending Email')
+		server.sendmail('#email', mail_ID ,msg)        #Enter Mail_Id again
+		tkinter.messagebox.showinfo('Congrats',"The Email has been sent!")
+		print("Email has been sent")
+		server.quit()
 
 def scrape():
-	print('Running Post-Scrape function')
-	now = datetime.now()
-	print(now)
+	print('Running Post-Scrape function', datetime.now())
 	source = requests.get(link).text
 	soup = BeautifulSoup(source, 'lxml')
-	# global price
 	price = soup.find('div', class_='_1vC4OE _3qQ9m1').text
 	temp_price = price[1:8].replace(',','')
 	global comp_price
 	comp_price = float(temp_price)
 	if(comp_price >= user_price):
-		print(f'{str(comp_price)} >= {str(user_price)}')
-		print('Product Not yet available')
+		print(f'{str(comp_price)} >= {str(user_price)}', 'Product Not yet available')
 	else:
 		print(f'Calling mail function: {str(comp_price)} < {str(user_price)}')
 		mail()
@@ -162,7 +158,7 @@ def radio():
  
 #UI Elements
 GUI = tk.ThemedTk()
-GUI.title("Flipkart Product Price")
+GUI.title("Flipkart Product Price Tracker")
 GUI.geometry("+160+80")
 GUI.get_themes()
 GUI.set_theme("scidblue")
@@ -171,7 +167,7 @@ GUI.maxsize(1000,700)
 
 frame1 = Frame(GUI, relief=GROOVE, bd=10, bg='deepskyblue')
 frame1.pack(fill=X)
-Welcome_text = Label(frame1, text='Flipkart Product Price', font=('times',30,'underline')
+Welcome_text = Label(frame1, text='Flipkart Product Price Tracker', font=('times',30,'underline')
 	                    , width=400, bg='steelblue', relief=RIDGE)
 Welcome_text.pack(fill=X, side=TOP)
 
